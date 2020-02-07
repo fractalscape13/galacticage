a2746ebfaaafd32478ca4a34125ca8f56f83bc45
Joseph Wangemann <fractalscape13@gmail.com>
2020-02-07 13:44:54 -0800

    update UI to take input from age form and health select option

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { Person } from './person';
+import { Person } from './person';
 
 $(document).ready(function() {
+  let user;
   $("#form").submit(function() {
     event.preventDefault();
+    user = new Person();
+    let age = $("input#age").val();
+    let health = $("#health").val();
+    user.setAge(age, health);
     $(".start").hide();
     $(".result").fadeIn();
+    console.log(user);
   });
 });
