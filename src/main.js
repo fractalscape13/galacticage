efc59c4f3e075ba72862b9f446934d83cfe7dec9
Joseph Wangemann <fractalscape13@gmail.com>
2020-02-07 13:16:37 -0800

    create hide/show function in main.js for start page and output page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import { Person } from './person';
+
+$(document).ready(function() {
+  $("#form").submit(function() {
+    event.preventDefault();
+    $(".start").hide();
+    $(".result").fadeIn();
+  });
+});
